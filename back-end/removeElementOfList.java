48a868497f3f38733d91703a0dc24f1bd5335766
xujiacheng <xujiacheng@foresee.com.cn>
2018-03-15 15:52:02 +0800

    遍历时正确删除满足条件的list中的元素

---
+++ back-end/removeElementOfList.java
@@ -0,0 +1,31 @@
+/**
+*
+*筛选_遍历时正确删除满足条件的list中的元素
+**/
+
+//错误代码-->出现异常ConcurrentModificationException(因为在遍历时remove会改变list 元素的index)
+List<Map<String, Object>> resMap = simpleJdbcTemplate.queryForList(...);
+List<Map<String, Object>> currentresMap = new ArrayList<Map<String, Object>>();
+for(Map<String, Object> temp : resMap) {
+    String day = (String)temp.get("MONTH");
+    String year = day.split("-")[0];
+    if(currentYear.equals(year)) {
+        currentresMap.add(temp);
+        resMap.remove(temp);
+    }
+}
+
+//正确代码(筛选)_迭代器
+ List<Integer> list1 = new ArrayList<Integer>();
+ for(int i = 1;i<=10;i++) {
+     list1.add(i);
+ }
+ List<Integer> list2 = new ArrayList<Integer>();
+  Iterator<Integer> iterator = list1.iterator();
+    while (iterator.hasNext()){
+        Integer next = iterator.next();
+        if(next%2==0) {
+           iterator.remove();
+           list2.add(next);
+        }
+    }
